b20574cf27af48176a2e3c8e197e88fc2661c939
Yuvraj Tamatta <yuvrajsinh123428@gmail.com>
2026-01-23 11:34:49 +0530

    Name

--- src/components/DownloadButton.tsx
+++ src/components/DownloadButton.tsx
@@ -6,8 +6,8 @@ const DownlaodButton = () => {
       href="/CS-39_ Introduction of Cloud Computing.pdf"
       download
     >
-      <button className="bg-primary hover:bg-primary/90 text-primary-foreground glow-primary w-full sm:w-auto justify-center">
-        Download PDF
+      <button className="ml-8 p-3 rounded-full bg-primary hover:bg-primary/90 text-primary-foreground glow-primary w-full sm:w-auto justify-center">
+        Download Material
       </button>
     </a>
   );
