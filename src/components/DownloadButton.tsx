62718580b22438b1d4771d5a926077b8df8c66e7
Yuvraj Tamatta <yuvrajsinh123428@gmail.com>
2026-01-28 11:43:53 +0530

    seo material upload

--- src/components/DownloadButton.tsx
+++ src/components/DownloadButton.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const DownlaodButton = () => {
   return (
     <a
-      href="/CS-39_ Introduction of Cloud Computing.pdf"
+      href="/CS – 10_ SAD, CH.2 Software Quality Assurance and Testing.pdf"
       download
     >
       <button className="ml-8 p-3 rounded-full bg-primary hover:bg-primary/90 text-primary-foreground glow-primary w-full sm:w-auto justify-center">
